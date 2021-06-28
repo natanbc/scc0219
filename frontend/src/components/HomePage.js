dbef86e099bc62c1e3bc42ff20af36b870661047
natanbc <natanbc@users.noreply.github.com>
2021-06-27 22:30:36 -0300

    make home page more responsive, change cursor on details button

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,9 +3,10 @@ import { SignIn } from './SignIn.js';
 export function HomePage({usersRepo}) {
     return <div>
         <div className="row">
-            <div className="col s6 offset-s1 flow-text">
+            <div className="col s12 m6 flow-text">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit lacus sit amet metus ullamcorper, at condimentum neque sagittis. Praesent euismod tortor nisi, eu lacinia elit volutpat et. Integer interdum libero id porttitor pulvinar. Nulla vel sagittis orci. Sed dignissim ex in massa tempus, a euismod risus consectetur. Proin et massa in magna tempus commodo. Phasellus et dui luctus, tincidunt est a, facilisis nibh. Quisque vel elit ac diam vehicula aliquam sit amet sit amet nunc. Aenean vestibulum eros sit amet urna scelerisque rutrum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus nec nibh efficitur, gravida lorem at, dignissim nisi. Vivamus nec ornare felis. Sed eu nibh volutpat, ultrices tellus a, ultricies nisl.
             </div>
+            <div className="col m1"></div>
             <SignIn usersRepo={usersRepo}></SignIn>
         </div>
     </div>;
--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -38,16 +38,16 @@ async function HandleLogin(event, usersRepo, authUserCtx) {
 export function SignIn({usersRepo}) {
     const authUserCtx = React.useContext(AuthUserContext);
 
-    return <form className="card col s3 offset-s1" onSubmit={(e) => HandleLogin(e, usersRepo, authUserCtx)}>
+    return <form className="card col s12 m5" onSubmit={(e) => HandleLogin(e, usersRepo, authUserCtx)}>
         <div className="card-content">
             <FormInput name="email" type="text" icon="email">
                 Email</FormInput>
             <FormInput name="password" type="password" icon="password">
                 Password</FormInput>
         </div>
-        <div className="card-action">
-            <Link href="/signup" className="btn-outlined waves-effect">SIGN UP</Link>
-            <button className="btn waves-effect waves-light right" type="submit" name="action">
+        <div className="card-action row">
+            <Link href="/signup" className="btn-outlined waves-effect col m5">SIGN UP</Link>
+            <button className="btn waves-effect waves-light right col m5" type="submit" name="action">
                 SIGN IN
                 <i className="material-icons right">login</i>
             </button>
