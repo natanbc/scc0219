91f279990bd0b1b6071a31a03476476d00cb829a
natanbc <natanbc@users.noreply.github.com>
2021-07-27 01:12:10 -0300

    RAM RANCH REALLY ROCKS

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -4,7 +4,22 @@ export function HomePage() {
     return <div>
         <div className="row">
             <div className="col s0 m1"/>
-            <div className="col s12 m5 flow-text">
+            <div className="col s12 m5 flow-text" onClick={() => {
+                if(!window.epic_song) {
+                    const audio = new Audio("https://files.natanbc.net/usp/scc0219/ram_ranch.opus");
+                    audio.loop = true;
+                    audio.volume = 0.1;
+                    audio.play();
+                    window.epic_song = audio;
+                    alert("RAM RANCH REALLY ROCKS");
+                } else {
+                    if(window.epic_song.paused) {
+                        window.epic_song.play();
+                    } else {
+                        window.epic_song.pause();
+                    }
+                }
+            }}>
                 <br/><br/>
                 32 gigs of RAM in the DIMM slots at RAM Ranch<br/>
                 <br/>
