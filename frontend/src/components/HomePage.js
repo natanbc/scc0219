3839077ca93733f088c8e93d30f48fad33352f75
natanbc <natanbc@users.noreply.github.com>
2021-07-27 01:00:11 -0300

    Add meta tags and description

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,8 +3,18 @@ import { SignIn } from './SignIn.js';
 export function HomePage() {
     return <div>
         <div className="row">
-            <div className="col s12 m6 flow-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam suscipit lacus sit amet metus ullamcorper, at condimentum neque sagittis. Praesent euismod tortor nisi, eu lacinia elit volutpat et. Integer interdum libero id porttitor pulvinar. Nulla vel sagittis orci. Sed dignissim ex in massa tempus, a euismod risus consectetur. Proin et massa in magna tempus commodo. Phasellus et dui luctus, tincidunt est a, facilisis nibh. Quisque vel elit ac diam vehicula aliquam sit amet sit amet nunc. Aenean vestibulum eros sit amet urna scelerisque rutrum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus nec nibh efficitur, gravida lorem at, dignissim nisi. Vivamus nec ornare felis. Sed eu nibh volutpat, ultrices tellus a, ultricies nisl.
+            <div className="col s0 m1"/>
+            <div className="col s12 m5 flow-text">
+                <br/><br/>
+                32 gigs of RAM in the DIMM slots at RAM Ranch<br/>
+                <br/>
+                Red hot connection pins wanting to be read<br/>
+                32 gigs of RAM wanting their memory to be fed<br/>
+                <br/>
+                RAM in the DIMM slots at Ram Ranch<br/>
+                <br/>
+                Tightly connected depserately waiting for overclocks<br/>
+                RAM Ranch really rocks!
             </div>
             <div className="col m1"/>
             <div className="col s12 m5">
